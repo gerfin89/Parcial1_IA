3e3e8d30c1fd554d91ae3011e71d8c4628d2aa50
gerfin89 <gerfinj.g@gmail.com>
2026-09-07 02:45:05 -0300

    Seek, Flee y Arrive con steering behaviors

--- Assets/Scripts/Boid/BoidSteering.cs
+++ Assets/Scripts/Boid/BoidSteering.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class BoidSteering : MonoBehaviour
 {
-    [SerializeField] private Transform target;
+    [SerializeField] private Transform _target;
     [SerializeField] private float maxSpeed;
     [SerializeField] private float maxSteering;
     [SerializeField] private float slowingDistance;
@@ -11,59 +11,82 @@ public class BoidSteering : MonoBehaviour
     [SerializeField] private float minDistance;
     private Vector3 _velocity; //currentVelocity
 
+    public enum steeringModes { Seek, Flee, Arrive, Evade, Pursuit}
+    public steeringModes currentSteering;
     
     void Update()
     {
-        //Evade();
-        //Seek();
-        //Flee();
-        Arrive();
-        
+        _velocity += SteeringVector();
+        _velocity = Vector3.ClampMagnitude(_velocity, maxSpeed);
         transform.position += _velocity * Time.deltaTime;
+        
+        if(_velocity != Vector3.zero )
         transform.forward = _velocity;
     }
 
-   
-    private void Seek()
+    private Vector3 SteeringVector()
     {
-        Vector3 desired = (target.position - transform.position).normalized;
-        desired *= maxSpeed;
-        Vector3 steering = desired - _velocity;
+        switch (currentSteering) 
+        {
+            case steeringModes.Seek:
+                return Seek(_target.position);
+            case steeringModes.Flee:
+                return Flee(_target.position);
+            case steeringModes.Arrive:
+                return Arrive();
+            case steeringModes.Evade:
+                return Vector3.zero;
+            default:
+                return Vector3.zero;
 
-        steering = Vector3.ClampMagnitude(steering, maxSteering*Time.deltaTime);
+        }
 
-        _velocity += steering;
     }
 
-    private void Flee()
+    private Vector3 CalculateSteering(Vector3 desired )
     {
-        Vector3 desired = (transform.position - target.position).normalized;
-        desired *= maxSpeed;
         Vector3 steering = desired - _velocity;
 
         steering = Vector3.ClampMagnitude(steering, maxSteering * Time.deltaTime);
+        return steering;
 
-        _velocity += steering;
+    }
+    private Vector3 DesiredVector(Vector3 target)
+    {
+        Vector3 desired = (target - transform.position).normalized;
+        desired *= maxSpeed;
+        return desired;
     }
 
-    private void Arrive()
+    private Vector3 Seek(Vector3 target)
     {
-        Vector3 direction = target.position - transform.position;
+       var desired = DesiredVector(target);
+
+       return CalculateSteering(desired);
+    }
+
+    private Vector3 Flee(Vector3 target)
+    {
+        var desired = DesiredVector(target);
+
+        return CalculateSteering(-desired);
+    }
+
+    private Vector3 Arrive()
+    {
+        Vector3 direction = _target.position - transform.position;
         float distance = direction.magnitude;
 
         if (distance < minDistance) 
         {
-            _velocity = Vector3.zero;
-            return;
+            return CalculateSteering (Vector3.zero);
+            
         }
 
         float targetSpeed = maxSpeed *(distance/slowingDistance);
         float desiredSpeed = Mathf.Min(targetSpeed,maxSpeed);
 
         Vector3 desired = direction.normalized * desiredSpeed;
-        Vector3 steering = desired - _velocity;
-
-        steering = Vector3.ClampMagnitude(steering,maxSteering*Time.deltaTime);
-        _velocity += steering;
+        return CalculateSteering(desired);
     }
 }
